1fbf44f9b2061b9906fb5c0d1230187dc0799801
“[firstname <jacbraq@dunwoody.edu>
2020-11-06 21:36:54 -0600

    finallygit add .git add .!

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ Name: Brant Jackson
 Date of completion:
 
 Construct a program that allows the end-user to input each employee’s name, department number, hourly salary, and number of hours worked. 
-Within each iteration of entering employee information, display the employee’s name, gross salary and department name.  
 Your program will need to defensively program to ensure that a correct department number is entered. 
 Declare an array that will hold the accumulated salary for each department and declare an array that holds each of the Department names. 
 Once the end-user concludes entering employee data, you will need to output is a list of the seven departments in the company and the total gross payroll (rate times hours) for each department. 
@@ -17,22 +16,17 @@ namespace assignment_three
         static void Main(string[] args)
         {
             string name;
-            double salary, hourly;
-            string [] departments = {"personnel", "marketing", "information technology", "computer service", "sales", "legal", "accounting"};
+            double salary, hours;
+            string [] departments = {"Personnel", "Marketing", "Information Technology", "Computer Service", "Sales", "Legal", "Accounting"};
+            double [] dptSalary = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
             // Trigger
             Console.WriteLine("Please enter department ID (0-personnel, 1-marketing, 2-infromation technology, 3-computer service, 4-sales, 5-legal, 6-accounting)");
             Console.WriteLine("9 to quit");
             int dptID = Convert.ToInt32(Console.ReadLine());
-            // multidimensional arrays
-            // double [,] dptSalaries = {dptID, salary};
 
-            // int [] personnelSalary, marketingSalary, infoTechSalary, compServiceSalary, salesSalary, legalSalary, acountingSalary;
-            // string [] personnelNames, marketingNames, infoTechNames, compServiceName, salesName, legalName, accountingName;
-
-
-            int END = 9;
+            int SENTINEL = 9;
             
-            while(dptID != END){
+            while(dptID != SENTINEL){
                 // Defensive Programming
                 while(dptID > 6 && dptID != 9){
                     Console.WriteLine("Incorrect, please enter department ID");
@@ -40,47 +34,23 @@ namespace assignment_three
                 }
 
                 if(dptID < 7){
-                Console.WriteLine("Please enter name:");
-                name = Console.ReadLine();
-                Console.WriteLine("Please enter hourly salary:");
-                hourly = Convert.ToDouble(Console.ReadLine());
-                salary = 52 * 40 * hourly;
-                Console.WriteLine("Name: " + name + " department: " + departments[dptID] + " salary: " + salary);
-
-                    if(dptID == 0){
-                        personnelNames [];
-                        personnelSalary [];
-                    }
-                    if(dptID == 1){
-                        marketingNames [];
-                        marketingSalary [];
-                    }
-                    if(dptID == 2){
-                        infoTechNames [];
-                        infoTechSalary [];
-                    }
-                    if(dptID == 3){
-                        compServiceName [];
-                        compServiceSalary [];
-                    }
-                    if(dptID == 4){
-                        salesName [];
-                        salesSalary [];
-                    }
-                    if(dptID == 5){
-                        legalName [];
-                        legalSalary [];
-                    }
-                    if(dptID == 6){
-                        accountingName [];
-                        acountingSalary [];
-                    }
+                    Console.WriteLine("Please enter name:");
+                    name = Console.ReadLine();
+                    Console.WriteLine("Please enter hours worked:");
+                    hours = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please enter hourly wage:");
+                    salary = Convert.ToDouble(Console.ReadLine());
+                    // Within each iteration of entering employee information, display the employee’s name, gross salary and department name.  
+                    Console.WriteLine(departments[dptID] + "Department member " + name + " earned $" + salary * hours);
+                    dptSalary[dptID] += salary * hours;
                 }
-
+      
                 Console.WriteLine("Please enter department ID");
                 dptID = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("goodbye");
+            for(int i = 0; i < 7; i++){
+            Console.WriteLine(departments[i] + " Department's total gross salary is " + dptSalary[i]);
+            }
         }
     }
 }
